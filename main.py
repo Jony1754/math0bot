1454d7524d94ed5e4fc450810c56556590db6487
Jonathan Arias Rua <jony1754@hotmail.com>
2021-05-20 16:42:26 -0500

    Corregidos errores en la logica de la secuencia y agregados algunos textos para el bot

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from telegram.ext import CommandHandler
 import telegram
 from my_graph import graph_img
 from secuencia import get_subsequence
+import random
 updater = Updater(
     token='1822223787:AAEl8_-tM3vxxTsTPSHqXQy4Ha0DW4tMdIc', use_context=True)
 dispatcher = updater.dispatcher
@@ -10,7 +11,7 @@ dispatcher = updater.dispatcher
 
 def help(update, context):
     context.bot.send_message(
-        chat_id=update.effective_chat.id, text="🤖 Hola, este bot fue creado por los mejores estudiantes de <b>Ingeniería de sistemas</b> de la Universidad Del Norte 👨🏻‍🎓👨🏻‍🎓👨🏿‍🎓. A continuación verás los comandos disponibles. \n1. 📉 /grafo V E K: Recibe como datos de entrada la cantidad de vértices de un grafo, la cantidad de aristas y el número máximo de aristas por vértice.\n 2. ♻ /recurrencia a b c d: Recibe los coeficientes de un polinomio caracteristico para una relacion de recurrencia \n3. 📏 /serie a b c d: Recibe una secuencia ORDENADA y determina una subsecuencia de la sucesion de <b>fibonacci</b>", parse_mode=telegram.ParseMode.HTML)
+        chat_id=update.effective_chat.id, text="🤖 Hola, este bot fue creado por los mejores estudiantes de <b>Ingeniería de sistemas</b> de la Universidad Del Norte 👨🏻‍🎓👨🏻‍🎓👨🏿‍🎓. A continuación verás los comandos disponibles. \n1. 📉 /grafo V E K: Recibe como datos de entrada la cantidad de vértices de un grafo, la cantidad de aristas y el número máximo de aristas por vértice.\n 2. ♻ /recurrencia a b c d: Recibe los coeficientes de un polinomio caracteristico para una relacion de recurrencia \n3. 📏 /secuencia a b c d: Recibe una secuencia ORDENADA y determina una subsecuencia de la sucesion de <b>fibonacci</b>", parse_mode=telegram.ParseMode.HTML)
 
 
 def secuencia(update, context):
@@ -19,22 +20,28 @@ def secuencia(update, context):
         if not item.isnumeric():
             update.message.reply_text(
                 "❌Ops, parece que uno de tus argumentos no es un número, intenta nuevamente.❌")
-            break
+            return
 
     # AGREGAR CADA ELEMENTO CADENA EN UNA LISTA NUMERICA
+
     arguments = [int(element) for element in context.args]
-    if is_ascendente(arguments):
-        resultado = get_subsequence(arguments, 0)
-        if len(resultado) > 0:
-            update.message.reply_text(
-                f"🤖Hey!, he encontrado esta secuencia: <b>{resultado}</b> \nNota que cada numero es el resultado de la suma de los dos inmediatamente anteriores y que todos ellos se encuentran en la secuencia que me diste. Genial, ¿No?", parse_mode=telegram.ParseMode.HTML)
+    if len(arguments) <= 1:
+        update.message.reply_text(
+            f"😒 Ingresa por lo menos 2 argumentos en forma <b>ascendente</b> para poder buscar una subsecuencia", parse_mode=telegram.ParseMode.HTML)
+        return
+    else:
+        if is_ascendente(arguments):
+            resultado = get_subsequence(arguments, random.randrange(2))
+            if len(resultado) > 0:
+                update.message.reply_text(
+                    f"🤖Hey!, he encontrado esta secuencia: <b>{resultado}</b> \nNota que cada numero es el resultado de la suma de los dos inmediatamente anteriores y que todos ellos se encuentran en la secuencia que me diste. Genial, ¿No?", parse_mode=telegram.ParseMode.HTML)
+            else:
+                update.message.reply_text(
+                    f"😢 No he podido encontrar una secuencia que tenga el flow de fibonacci, lo siento. ", parse_mode=telegram.ParseMode.HTML)
+
         else:
             update.message.reply_text(
-                f"😢 No he podido encontrar una secuencia que tenga el flow de fibonacci, lo siento. ", parse_mode=telegram.ParseMode.HTML)
-            pass
-    else:
-        update.message.reply_text(
-            "❌Lo siento, la secuencia debe ser <b>ascendente</b>📉", parse_mode=telegram.ParseMode.HTML)
+                "❌Lo siento, la secuencia debe ser <b>ascendente</b>📉", parse_mode=telegram.ParseMode.HTML)
 
 
 def is_ascendente(sequence):
--- secuencia.py
+++ secuencia.py
@@ -7,6 +7,9 @@
 
 def get_subsequence(sequence, starting_index=0):
     subsequence = []
+    if starting_index > len(sequence) - 1:
+        return ValueError
+
     for item in range(starting_index, len(sequence) - 1, 1):
         curr = sequence[item]
         sig = sequence[item + 1]
@@ -17,7 +20,7 @@ def get_subsequence(sequence, starting_index=0):
             subsequence.append(suma)
             break
 
-    for subitem in range(0, len(subsequence) - 1, 1):
+    for subitem in range(0, len(subsequence), 1):
         curr2 = subsequence[len(subsequence) - 1]
         sig2 = subsequence[len(subsequence) - 2]
         suma2 = curr2 + sig2
@@ -27,8 +30,7 @@ def get_subsequence(sequence, starting_index=0):
     return subsequence
 
 
-# main([0, 2, 3, 4, 5, 7, 11, 13, 18, 22, 28], 0)
-# main([1, 2, 3, 4, 5, 6, 7, 8], 0)
+# print(get_subsequence([2, 3, 4, 5, 7, 11, 13, 18, 22, 29], 1))
 # PARA EL EJEMPLO SE A;ADE ASID
 # 0 2 2
 # 0 2 2 4 -> 6 NO ESTA Y AQUI SE DETIENE
